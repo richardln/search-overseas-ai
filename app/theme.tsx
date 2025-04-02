3780fc598acaf9cc9edccbf6b516664841e0a429
richardln <43290124+richardln@users.noreply.github.com>
2025-04-02 20:27:33 +0800

    Update theme.tsx

--- app/theme.tsx
+++ app/theme.tsx
@@ -1,1 +1,31 @@
+import { useEffect } from 'react';
+
+// 监听用户系统主题变化
+const useTheme = () => {
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // 根据初始设置应用对应的主题
+    if (darkModeMediaQuery.matches) {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+    }
+
+    // 监听系统主题变化
+    darkModeMediaQuery.addEventListener('change', (e) => {
+      if (e.matches) {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+      } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+      }
+    });
+  }, []);
+};
+
+export default useTheme;
 
